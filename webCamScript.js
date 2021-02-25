ea644a97ed9ad61ce3d5b1f810faf730ea1c12e7
Matthew Heywood <matthewheywood@live.co.uk>
2021-02-25 11:45:30 +0000

    update upload file code

--- webCamScript.js
+++ webCamScript.js
@@ -39,44 +39,20 @@ let current = new Date();
 
 // *********** Upload file to Cloudinary ******************** //
 function uploadFile(file) {
-  var url = `https://api.cloudinary.com/v1_1/dbl3jetzn/upload`;
-  var xhr = new XMLHttpRequest();
-  var fd = new FormData();
-  xhr.open('POST', url, true);
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-  // Reset the upload progress bar
-   document.getElementById('progress').style.width = 0;
-  
-  // Update progress (can be used to show progress indicator)
-  xhr.upload.addEventListener("progress", function(e) {
-    var progress = Math.round((e.loaded * 100.0) / e.total);
-    document.getElementById('progress').style.width = progress + "%";
-
-    console.log(`fileuploadprogress data.loaded: ${e.loaded},
-  data.total: ${e.total}`);
-  });
-
-  xhr.onreadystatechange = function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // File uploaded successfully
-      var response = JSON.parse(xhr.responseText);
-      // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
-      var url = response.secure_url;
-      // Create a thumbnail of the uploaded image, with 150px width
-      var tokens = url.split('/');
-      tokens.splice(-2, 0, 'w_150,c_scale');
-      var img = new Image(); // HTML5 Constructor
-      img.src = tokens.join('/');
-      img.alt = response.public_id;
-      console.log(img);
-    }
-  };
-
-  fd.append('upload_preset', 'dundtgidc');
-  fd.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
-  fd.append('file', file);
-  xhr.send(fd);
+  $.ajax({
+        type    : "POST",
+        url     : "`https://api.cloudinary.com/v1_1/dbl3jetzn/upload`",
+        beforeSend: function(xhr){xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');},
+        data    : {'upload_preset':'dundtgidc','tags': 'browser_upload' ,'file': file},
+        success: function(data){
+            console.log(data);
+        },
+        error: function(xhr, status, error) {
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        }
+    });
 }
 
 // Take a picture when cameraTrigger is tapped
